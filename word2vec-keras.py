00ef01de39a59ad8474a9561afd1cb43cf559518
SimonHegelich <simonhegelich@ddductr.de>
2019-07-02 09:34:32 +0200

    keras skipgram word2vec tensorflow2.0

---
+++ word2vec-keras.py
@@ -0,0 +1,162 @@
+# based in parts on https://adventuresinmachinelearning.com/word2vec-tutorial-tensorflow/
+# and https://adventuresinmachinelearning.com/word2vec-keras-tutorial/
+# rewritten for tensorflow 2.0.1beta
+
+import os
+import urllib
+import tensorflow as tf
+import zipfile
+import collections
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+import io
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+url = 'http://mattmahoney.net/dc/'
+filename = maybe_download('text8.zip', url, 31344016)
+
+# Read the data into a list of strings.
+def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words."""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+vocabulary = read_data(filename)
+print(vocabulary[:7])
+
+def build_dataset(words, n_words):
+    """Process raw inputs into a dataset."""
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
+
+vocab_size=100
+data, count, dictionary, reverse_dictionary = build_dataset(vocabulary, vocab_size)
+
+window_size = 3
+vector_dim = 3
+epochs = 100
+
+valid_size = 16     # Random set of words to evaluate similarity on.
+valid_window = 100  # Only pick dev samples in the head of the distribution.
+valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+
+sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(vocab_size)
+couples, labels = tf.keras.preprocessing.sequence.skipgrams(data, vocab_size, window_size=window_size, sampling_table=sampling_table)
+word_target, word_context = zip(*couples)
+word_target = np.array(word_target, dtype="int32")
+word_context = np.array(word_context, dtype="int32")
+
+print(couples[:10], labels[:10])
+
+# create some input variables
+input_target = keras.Input((1,))
+input_context = keras.Input((1,))
+
+embedding = layers.Embedding(vocab_size, vector_dim, input_length=1, name='embedding')
+
+target = embedding(input_target)
+target = layers.Reshape((vector_dim, 1))(target)
+context = embedding(input_context)
+context = layers.Reshape((vector_dim, 1))(context)
+
+# setup a cosine similarity operation which will be output in a secondary model
+similarity = layers.dot(inputs=[target, context], axes=0, normalize=True)
+
+# now perform the dot product operation to get a similarity measure
+dot_product = layers.dot(inputs=[target, context], axes=1)
+dot_product = layers.Reshape((1,))(dot_product)
+# add the sigmoid output layer
+output = layers.Dense(1, activation='sigmoid')(dot_product)
+
+# create the primary training model
+model = keras.Model(inputs=[input_target, input_context], outputs=output)
+print(model.summary())
+model.compile(loss='binary_crossentropy', optimizer='rmsprop')
+
+# create a secondary validation model to run our similarity checks during training
+validation_model = keras.Model(inputs=[input_target, input_context], outputs=similarity)
+
+class SimilarityCallback:
+    def run_sim(self):
+        for i in range(valid_size):
+            valid_word = reverse_dictionary[valid_examples[i]]
+            top_k = 8  # number of nearest neighbors
+            sim = self._get_sim(valid_examples[i])
+            nearest = (-sim).argsort()[1:top_k + 1]
+            log_str = 'Nearest to %s:' % valid_word
+            for k in range(top_k):
+                close_word = reverse_dictionary[nearest[k]]
+                log_str = '%s %s,' % (log_str, close_word)
+            print(log_str)
+
+    @staticmethod
+    def _get_sim(valid_word_idx):
+        sim = np.zeros((vocab_size,))
+        in_arr1 = np.zeros((1,))
+        in_arr2 = np.zeros((1,))
+        for i in range(vocab_size):
+            in_arr1[0,] = valid_word_idx
+            in_arr2[0,] = i
+            out = validation_model.predict_on_batch([in_arr1, in_arr2])
+            sim[i] = out
+        return sim
+sim_cb = SimilarityCallback()
+
+arr_1 = np.zeros((1,))
+arr_2 = np.zeros((1,))
+arr_3 = np.zeros((1,))
+for cnt in range(epochs):
+    idx = np.random.randint(0, len(labels)-1)
+    arr_1[0,] = word_target[idx]
+    arr_2[0,] = word_context[idx]
+    arr_3[0,] = labels[idx]
+    loss = model.train_on_batch([arr_1, arr_2], arr_3)
+    if cnt % 100 == 0:
+        print("Iteration {}, loss={}".format(cnt, loss))
+    if cnt % 10000 == 0:
+        sim_cb.run_sim()
+
+e = model.layers[2]
+weights = e.get_weights()[0]
+print(weights.shape) # shape: (vocab_size, embedding_dim)
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(vocab_size):
+  word = reverse_dictionary[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+# load vecs.tsv and meta.tsv in projector.tensorflow.org
